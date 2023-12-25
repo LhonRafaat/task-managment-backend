bdb8496b98149bcbf02af306f56b52664619fdeb
lhonrafaat <lhontaekwondo@gmail.com>
2023-12-25 22:17:06 +0300

    import users module into task module

--- src/modules/task/task.module.ts
+++ src/modules/task/task.module.ts
@@ -3,9 +3,14 @@ import { TaskService } from './task.service';
 import { TaskController } from './task.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TaskSchema } from './task.schema';
+import { UsersService } from '../users/users.service';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'Task', schema: TaskSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: 'Task', schema: TaskSchema }]),
+    UsersModule,
+  ],
   controllers: [TaskController],
   providers: [TaskService],
 })
