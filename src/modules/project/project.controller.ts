115897ce11ad270ee15440ee2ba200267494a8cb
lhonrafaat <lhontaekwondo@gmail.com>
2024-02-24 23:40:56 +0300

    added typing for project

--- src/modules/project/project.controller.ts
+++ src/modules/project/project.controller.ts
@@ -13,9 +13,14 @@ import {
 import { ProjectService } from './project.service';
 import { CreateProjectDto } from './dto/create-project.dto';
 import { UpdateProjectDto } from './dto/update-project.dto';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { AuthGuard } from '@nestjs/passport';
-import { IQuery, IRequest } from '../../common/helper/common-types';
+import {
+  IQuery,
+  IRequest,
+  getResponseType,
+} from '../../common/helper/common-types';
+import { TProject } from './models/project.model';
 
 @Controller('project')
 @ApiTags('Project')
@@ -24,22 +29,31 @@ export class ProjectController {
   constructor(private readonly projectService: ProjectService) {}
 
   @Post()
+  @ApiOkResponse({
+    type: TProject,
+  })
+  @UseGuards(AuthGuard('jwt'))
   create(@Body() createProjectDto: CreateProjectDto, @Req() req: IRequest) {
     return this.projectService.create(createProjectDto, req);
   }
 
   @Get()
   @UseGuards(AuthGuard('jwt'))
+  @ApiOkResponse(getResponseType(TProject))
   findAll(@Req() req: IRequest, @Query() query: IQuery) {
     return this.projectService.findAll(query, req);
   }
 
   @Get(':id')
+  @UseGuards(AuthGuard('jwt'))
+  @ApiOkResponse(getResponseType(TProject))
   findOne(@Param('id') id: string) {
     return this.projectService.findOne(id);
   }
 
   @Patch(':id')
+  @UseGuards(AuthGuard('jwt'))
+  @ApiOkResponse(getResponseType(TProject))
   update(@Param('id') id: string, @Body() updateProjectDto: UpdateProjectDto) {
     return this.projectService.update(id, updateProjectDto);
   }
