3360d8c7ea1929cee0e5261cf04357a4be032529
lhonrafaat <lhontaekwondo@gmail.com>
2024-04-12 22:53:50 +0300

    imported user invitation model to  module imports

--- src/user-invitations/user-invitations.module.ts
+++ src/user-invitations/user-invitations.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { UserInvitationsService } from './user-invitations.service';
 import { UserInvitationsController } from './user-invitations.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserInvitation } from './user-invitation.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: 'UserInvitation', schema: UserInvitation },
+    ]),
+  ],
   controllers: [UserInvitationsController],
-  providers: [UserInvitationsService]
+  providers: [UserInvitationsService],
 })
 export class UserInvitationsModule {}
