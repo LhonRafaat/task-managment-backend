cfe3b132d5fcb6d3e17b132606ef48b55397eec3
lhonrafaat <lhontaekwondo@gmail.com>
2024-04-15 14:17:13 +0300

    fixed app url

--- src/main.ts
+++ src/main.ts
@@ -35,7 +35,7 @@ async function bootstrap() {
   app.enableCors();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
-  await app.listen(configService.get('PORT'));
+  await app.listen(configService.get('PORT'), '0.0.0.0');
   console.log(`Application running at ${await app.getUrl()}`);
 }
 bootstrap();
