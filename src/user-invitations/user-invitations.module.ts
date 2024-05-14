ab8d47fea6408aa977764f08ccc19e0321bcc01b
lhonrafaat <lhontaekwondo@gmail.com>
2024-05-15 01:07:13 +0300

    added email template and added checks to see if user exist to send a different invitation link

--- src/user-invitations/user-invitations.module.ts
+++ src/user-invitations/user-invitations.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { UserInvitationsService } from './user-invitations.service';
 import { UserInvitationsController } from './user-invitations.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserInvitation } from './user-invitation.schema';
 import { OrganizationModule } from '../modules/organization/organization.module';
+import { UsersModule } from '../modules/users/users.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { OrganizationModule } from '../modules/organization/organization.module'
       { name: 'UserInvitation', schema: UserInvitation },
     ]),
     OrganizationModule,
+    forwardRef(() => UsersModule),
   ],
   controllers: [UserInvitationsController],
   providers: [UserInvitationsService],
--- src/user-invitations/user-invitations.service.ts
+++ src/user-invitations/user-invitations.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, forwardRef } from '@nestjs/common';
 import { CreateUserInvitationDto } from './dto/create-user-invitation.dto';
 import { UpdateUserInvitationDto } from './dto/update-user-invitation.dto';
 import { InjectModel } from '@nestjs/mongoose';
@@ -8,6 +8,7 @@ import { IRequest } from '../common/helper/common-types';
 import { MailerService } from '@nestjs-modules/mailer';
 import { ConfigService } from '@nestjs/config';
 import { OrganizationService } from '../modules/organization/organization.service';
+import { UsersService } from '../modules/users/users.service';
 
 @Injectable()
 export class UserInvitationsService {
@@ -17,6 +18,8 @@ export class UserInvitationsService {
     private readonly mailerService: MailerService,
     private readonly configService: ConfigService,
     private readonly organizationService: OrganizationService,
+    @Inject(forwardRef(() => UsersService))
+    private readonly usersService: UsersService,
   ) {}
 
   async create(
@@ -34,16 +37,44 @@ export class UserInvitationsService {
           new Date().toString() + 7 * 24 * 60 * 60 * 1000,
         ).toISOString(),
       });
-
+      let url =
+        this.configService.get('FRONTEND_URL') + `invite?id=` + invitation._id;
+      const user = await this.usersService.findByEmail(email);
+      if (user) {
+        url =
+          this.configService.get('FRONTEND_URL') +
+          `invite/accept?id=` +
+          invitation._id;
+      }
       this.mailerService
         .sendMail({
           to: email, // list of receivers
           from: 'noreply@taskmanagment.com', // sender address
           subject: `بانگێشت کراوی بۆ ${organization.title}`, // Subject line
-          text:
-            this.configService.get('FRONTEND_URL') +
-            `invite?id=` +
-            invitation._id,
+          html: `<body dir="rtl" style="font-family: Arial, sans-serif; margin: 0; padding: 0;">
+
+<!-- Header Section -->
+<div style="background-color: #f5f5f5; padding: 20px;">
+   TSM
+</div>
+
+<!-- Content Section -->
+<div style="padding: 20px;">
+    <h2 style="color: #333;">بەشدار بە لە رێکخراوەکەمان!</h2>
+    <p style="color: #666;">بەڕێز ${email}،</p>
+    <p style="color: #666;">ئێمە بانگێشت لێ دەکەین کە بەشداری  بکەیت لە ڕێکخراوەکەمان . وەک ئەندامێک، دەتوانیت سوود و دەرفەتی تایبەتت هەبێت بۆ هاوکاریکردن لەگەڵ کەسانی هاوشێوە.</p>
+    <p style="color: #666;">بۆ قبوڵکردن تکایە ئەو دوگمەیەی خوارەوە دابگرە</p>
+    <a href="${url}" style="display: inline-block; background-color: #007bff; color: #fff; text-decoration: none; padding: 10px 20px; border-radius: 5px; margin-top: 20px;">قبوڵکردن</a>
+    <p style="color: #666; margin-top: 20px;">ئەگەر هەر پرسیارێک یاخود هاوکاریەک پێویست بوو تکایە پەیوەندیمان پێوە بکە.</p>
+    <p style="color: #666; font-size: 48px;">${organization.title},</p>
+</div>
+
+<!-- Footer Section -->
+<div style="background-color: #f5f5f5; padding: 20px; text-align: center;">
+    <p style="color: #666; margin: 0;">© 2024 TSM. All rights reserved.</p>
+</div>
+
+</body>`,
         })
         .then(() => console.log('Email sent'))
         .catch((e) => console.log(e));
